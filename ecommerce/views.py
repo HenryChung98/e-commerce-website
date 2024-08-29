adede05b3397bddaff2bcbcb95571001f376ae59
정현규 <tongsik98@gmail.com>
2024-08-29 16:25:44 -0700

    optimized

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,19 +4,21 @@ from allauth.account.views import PasswordChangeView
 from django.contrib.auth.decorators import login_required
 import stripe
 from django.views.generic import UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.conf import settings
 
 from .models import User, Item, Order, OrderItem, Cart, CartItem, Review
 from .forms import ReviewForm, OrderForm, PaymentForm, SignupForm
 
-def index(request):
+# get sum of review rate / number of reviews
+def review_avg():
     items = Item.objects.all()
     reviews = Review.objects.all()
-    
-    # ===== get review avg =====
+
     review_sum = {}
     review_num = {}
     review_avg = {}
+    
     for item in items:
         review_sum[item.id] = 0 
         review_num[item.id] = 0 
@@ -30,56 +32,52 @@ def index(request):
             review_avg[key] = float(review_sum[key]) / float(review_num[key])
         except:
             review_avg[key] = 0
-    # ===== get review avg =====
+
+    return review_avg
+
+
+def index(request):
+    items = Item.objects.all()
+    reviews = Review.objects.all()
 
     context = {
         "items": items,
         "reviews": reviews,
-        "review_avg": review_avg
+        "review_avg": review_avg()
         }
     return render(request, "ecommerce/index.html", context)
 
+
+# password change view
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
         return reverse("index")
 
 
-# items 
+# ==================== items views ====================
 def item_list(request):
     items = Item.objects.all()
     reviews = Review.objects.all()
 
-    # ===== get review avg =====
-    review_sum = {}
-    review_num = {}
-    review_avg = {}
-    for item in items:
-        review_sum[item.id] = 0 
-        review_num[item.id] = 0 
-    
-    for review in reviews:
-        review_sum[review.item.id] += review.rate
-        review_num[review.item.id] += 1
-
-    for key in review_sum:
-        try:
-            review_avg[key] = float(review_sum[key]) / float(review_num[key])
-        except:
-            review_avg[key] = 0
-    # ===== get review avg =====
-
     context = {
         "items": items,
         "reviews": reviews,
-        "review_avg": review_avg
+        "review_avg": review_avg()
         }
     return render(request, "ecommerce/item_list_templates/item_list.html", context)
+
 
 def item_detail(request, item_id):
     item = Item.objects.get(id=item_id)
     reviews = Review.objects.filter(item=item)
-    context = {"item": item,
-                "reviews": reviews
+    is_reviewed = reviews.filter(user=request.user).exists() if request.user.is_authenticated else False
+    my_review = reviews.filter(user=request.user).first() if is_reviewed else ""
+
+    context = {
+        "item": item,
+        "reviews": reviews,
+        "is_reviewed": is_reviewed,
+        "my_review": my_review
             }
     return render(request, "ecommerce/item_detail.html", context)
 
@@ -88,34 +86,14 @@ def search(request):
     query = request.GET.get('q')
     items = Item.objects.filter(name__icontains=query)
     reviews = Review.objects.all()
-    
-    # ===== get review avg =====
-    review_sum = {}
-    review_num = {}
-    review_avg = {}
-    for item in items:
-        review_sum[item.id] = 0 
-        review_num[item.id] = 0 
-    
-    for review in reviews:
-        review_sum[review.item.id] += review.rate
-        review_num[review.item.id] += 1
-
-    for key in review_sum:
-        try:
-            review_avg[key] = float(review_sum[key]) / float(review_num[key])
-        except:
-            review_avg[key] = 0
-    # ===== get review avg =====
 
     context = {
         "items": items,
         "reviews": reviews,
-        "review_avg": review_avg,
+        "review_avg": review_avg(),
         'query': query
         }
 
-    
     return render(request, 'ecommerce/item_list_templates/search_results.html', context)
 
 
@@ -126,40 +104,24 @@ def item_category(request, category=None):
         items = Item.objects.all()
 
     reviews = Review.objects.all()
-    # ===== get review avg =====
-    review_sum = {}
-    review_num = {}
-    review_avg = {}
-    for item in items:
-        review_sum[item.id] = 0 
-        review_num[item.id] = 0 
-    
-    for review in reviews:
-        review_sum[review.item.id] += review.rate
-        review_num[review.item.id] += 1
-
-    for key in review_sum:
-        try:
-            review_avg[key] = float(review_sum[key]) / float(review_num[key])
-        except:
-            review_avg[key] = 0
-    # ===== get review avg =====
 
     context = {
         "items": items,
         "reviews": reviews,
-        "review_avg": review_avg
+        "review_avg": review_avg()
         }
     return render(request, 'ecommerce/item_list_templates/item_category_results.html', context)
 
-# cart
+# ==================== /items views ====================
+
+# ==================== cart handling ====================
 @login_required
 def cart_list(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
     cart_items = CartItem.objects.filter(cart=cart)
     total_amount = sum(item.quantity * item.price for item in cart_items)
 
-    cart_items_data = [
+    request.session['cart_items'] = [
         {
             'item_id': item.item.id,
             'item_name': item.item.name,
@@ -168,7 +130,7 @@ def cart_list(request):
         }
         for item in cart_items
     ]
-    request.session['cart_items'] = cart_items_data
+
     context = {
         "cart": cart,
         "cart_items": cart_items,
@@ -179,33 +141,20 @@ def cart_list(request):
 
 @login_required
 def add_to_cart(request, item_id):
-    # Get or create a cart for the user
     cart, created = Cart.objects.get_or_create(user=request.user)
-
-    # Get the item to be added to the cart
     item = get_object_or_404(Item, id=item_id)
 
-    # Check if the item is already in the cart
     cart_item, created = CartItem.objects.get_or_create(
         item=item,
         cart=cart,
         defaults={'quantity': 1, 'price': item.price}
     )
-    
+
     if not created:
-        # If the item already exists in the cart, update the quantity
         cart_item.quantity += 1
         cart_item.save()
-    reviews = Review.objects.filter(item=item)
 
-    context = {
-        "item": item,
-        "reviews": reviews,
-        "cart": cart,
-        "cart_items": CartItem.objects.filter(cart=cart),
-    }
-
-    return render(request, "ecommerce/item_detail.html", context)
+    return redirect('item-detail', item_id=item_id)
 
 
 @login_required
@@ -233,9 +182,9 @@ def remove_item(request, item_id):
     cart_item = get_object_or_404(CartItem, cart=cart, item_id=item_id)
     cart_item.delete()
     return redirect('cart-list')
+# ==================== /cart handling ====================
 
-
-# profile
+# profile view
 @login_required
 def user_profile(request, user_id):
     user = get_object_or_404(User, id=user_id)
@@ -252,8 +201,7 @@ def user_profile(request, user_id):
     return render(request, "ecommerce/user_profile.html", context)
 
 
-
-# forms
+# ==================== form handling ====================
 @login_required
 def create_review(request, item_id):
     item = Item.objects.get(id=item_id)
@@ -286,8 +234,14 @@ class ReviewUpdateView(UpdateView):
     template_name = 'forms/review_form.html'
     pk_url_kwarg = 'review_id'
 
+    raise_exception = True
+
     def get_success_url(self):
         return reverse('item-detail', kwargs={'item_id': self.object.item.id})
+    
+    # def test_func(self):
+    #     review = self.get_object()
+    #     return review.user == self.request.user
 
 
 class ReviewDeleteView(DeleteView):
@@ -295,9 +249,14 @@ class ReviewDeleteView(DeleteView):
     pk_url_kwarg = 'review_id'
     context_object_name = 'review'
 
+    raise_exception = True
+
     def get_success_url(self):
         return reverse('item-detail', kwargs={'item_id': self.object.item.id})
     
+    # def test_func(self):
+    #     review = self.get_object()
+    #     return review.user == self.request.user
 
 @login_required
 def order_form(request):
@@ -339,7 +298,6 @@ def order_form(request):
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def process_payment(request):
-    # 세션에서 Order 데이터 가져오기
     order_data = request.session.get('order_data')
     if not order_data:
         return redirect('cart-list')
@@ -394,3 +352,4 @@ def process_payment(request):
 
     return render(request, 'forms/process_payment.html', {'form': form})
 
+# ==================== /form handling ====================
